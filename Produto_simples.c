030767a71535beee4ffdb304fad0be86830c91d0
Gildean <gmonteiro0808@gmail.com>
2025-04-27 16:17:06 -0300

    Programando produto simples

---
+++ Produto_simples.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+ 
+int main() {
+ 
+    int n1, n2, prod;
+     scanf ("%i %i", &n1,&n2);
+     
+    prod = n1 * n2;
+    
+        printf("Produto = %i\n", prod);
+     
+    return 0;
+}
