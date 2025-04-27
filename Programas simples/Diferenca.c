2628f328d067730164e23d6c1db4a1027acef6b9
Gildean <gmonteiro0808@gmail.com>
2025-04-27 16:49:56 -0300

    Programando diferença

---
+++ Programas simples/Diferenca.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+   
+    int valor1, valor2, valor3, valor4, direfenca;
+    //É possivel utilizar tanto %i quanto %d para escanear uma variavel.
+    scanf("%i %i", &valor1, &valor2); 
+    scanf("%d %d", &valor3, &valor4);
+
+    diferenca = valor1*valor2 - valor3*valor4;
+    printf("Diferença = %d\n", diferenca);
+
+    return 0;
+
+}
