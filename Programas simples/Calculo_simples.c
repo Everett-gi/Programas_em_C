54097208e53bbc041ca83200da6a100bb94607c0
Gildean.Everett <gmonteiro0808@gmail.com>
2025-07-21 16:52:57 -0300

    criando calculo_simples

---
+++ Programas simples/Calculo_simples.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+ 
+int main() {
+ 
+   double codigoPeca1, quantidadePeca1, valorPeca1;
+   double codigoPeca2, QuantidadePeca2, valorPeca2, valorASerPago;
+    scanf ("%lf%lf%lf", &codigoPeca1,&quantidadePeca1,&valorPeca1);
+    scanf ("%lf%lf%lf", &codigoPeca2,&quantidadePeca2,&valorPeca2);
+ 
+ valorASerPago = quantidadePeca1 * valorPeca1 + quantidadePeca2 * valorPeca2;
+ 
+ printf ("VALOR A PAGAR: R$ %.2lf\n", valorASerPago);
+    return 0;
+}
