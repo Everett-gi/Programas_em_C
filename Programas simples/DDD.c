501f637e3d77c91ba750e0b077830582d09325dc
Gildean.Everett <gmonteiro0808@gmail.com>
2025-07-23 13:19:47 -0300

    programando DDD.c

---
+++ Programas simples/DDD.c
@@ -0,0 +1,216 @@
+#include <stdio.h>
+
+int main() {
+
+    int ddd;
+    scanf("%i", &ddd);
+    switch (ddd)
+    {    
+        case 11:{
+            printf("Sao Paulo (SP)\n");
+            break;
+        } case 12:{
+            printf("Sao Jose dos Campos (SP)\n");
+            break;
+        } case 13:{
+            printf("Santos (SP)\n");
+            break;
+        } case 14:{
+            printf("Bauru (SP)\n");
+            break;
+        } case 15:{
+            printf("Sorocaba (SP)\n");
+            break;
+        } case 16:{
+            printf("Ribeirao Preto (SP)\n");
+            break;
+        } case 17:{
+            printf("Sao Jose do Rio Preto (SP)\n");
+            break;
+        } case 18:{
+            printf("Presidente Prudente (SP)\n");
+            break;
+        } case 19:{
+            printf("Campinas (SP)\n");
+            break;
+        } case 21:{
+            printf("Rio de Janeiro (RJ)\n");
+            break;
+        } case 22:{
+            printf("Campos dos Goytacazes (RJ)\n");
+            break;
+        } case 24:{
+            printf("Volta Redonda (RJ)\n");
+            break;
+        } case 27:{
+            printf("Vitoria (ES)\n");
+            break;
+        } case 28:{
+            printf("Cachoeiro de Itapemirim (ES)\n");
+            break;
+        } case 31:{
+            printf("Belo Horizonte (MG)\n");
+            break;
+        } case 32:{
+            printf("Juiz de Fora (MG)\n");
+            break;
+        } case 33:{
+            printf("Governador Valadares (MG)\n");
+            break;
+        } case 34:{
+            printf("Uberlandia (MG)\n");
+            break;
+        } case 35:{
+            printf("Pocos de Caldas (MG)\n");
+            break;
+        } case 37:{
+            printf("Divinopolis (MG)\n");
+            break;
+        } case 38:{
+            printf("Montes Claros (MG)\n");
+            break;
+        } case 41:{
+            printf("Curitiba (PR)\n");
+            break;
+        } case 42:{
+            printf("Ponta Grossa (PR)\n");
+            break;
+        } case 43:{
+            printf("Londrina (PR)\n");
+            break;
+        } case 44:{
+            printf("Maringa (PR)\n");
+            break;
+        } case 45:{
+            printf("Foz do Iguacu (PR)\n");
+            break;
+        } case 46:{
+            printf("Francisco Beltrao (PR)\n");
+            break;
+        } case 47:{
+            printf("Joinville (SC)\n");
+            break;
+        } case 48:{
+            printf("Florianopolis (SC)\n");
+            break;
+        } case 49:{
+            printf("Chapeco (SC)\n");
+            break;
+        } case 51:{
+            printf("Porto Alegre (RS)\n");
+            break;
+        } case 53:{
+            printf("Pelotas (RS)\n");
+            break;
+        } case 54:{
+            printf("Caxias do Sul (RS)\n");
+            break;
+        } case 55:{
+            printf("Santa Maria (RS)\n");
+            break;
+        } case 61:{
+            printf("Brasilia (DF)\n");
+            break;
+        } case 62:{
+            printf("Goiania (GO)\n");
+            break;
+        } case 63:{
+            printf("Palmas (TO)\n");
+            break;
+        } case 64:{
+            printf("Rio Verde (GO)\n");
+            break;
+        } case 65:{
+            printf("Cuiaba (MT)\n");
+            break;
+        } case 66:{
+            printf("Rondonopolis (MT)\n");
+            break;
+        } case 67:{
+            printf("Campo Grande (MS)\n");
+            break;
+        } case 68:{
+            printf("Rio Branco (AC)\n");
+            break;
+        } case 69:{
+            printf("Porto Velho (RO)\n");
+            break;
+        } case 71:{
+            printf("Salvador (BA)\n");
+            break;
+        } case 73:{
+            printf("Ilheus (BA)\n");
+            break;
+        } case 74:{
+            printf("Juazeiro (BA)\n");
+            break;
+        } case 75:{
+            printf("Feira de Santana (BA)\n");
+            break;
+        } case 77:{
+            printf("Barreiras (BA)\n");
+            break;
+        } case 79:{
+            printf("Aracaju (SE)\n");
+            break;
+        } case 81:{
+            printf("Recife (PE)\n");
+            break;
+        } case 82:{
+            printf("Maceio (AL)\n");
+            break;
+        } case 83:{
+            printf("Joao Pessoa (PB)\n");
+            break;
+        } case 84:{
+            printf("Natal (RN)\n");
+            break;
+        } case 85:{
+            printf("Fortaleza (CE)\n");
+            break;
+        } case 86:{
+            printf("Teresina (PI)\n");
+            break;
+        } case 87:{
+            printf("Petrolina (PE)\n");
+            break;
+        } case 88:{
+            printf("Juazeiro do Norte (CE)\n");
+            break;
+        } case 89:{
+            printf("Picos (PI)\n");
+            break;
+        } case 91:{
+            printf("Belem (PA)\n");
+            break;
+        } case 92:{
+            printf("Manaus (AM)\n");
+            break;
+        } case 93:{
+            printf("Santarem (PA)\n");
+            break;
+        } case 94:{
+            printf("Maraba (PA)\n");
+            break;
+        } case 95:{
+            printf("Boa Vista (RR)\n");
+            break;
+        } case 96:{
+            printf("Macapa (AP)\n");
+            break;
+        } case 97:{
+            printf("Coari (AM)\n");
+            break;
+        } case 98:{
+            printf("Sao Luis (MA)\n");
+            break;
+        } case 99:{
+            printf("Imperatriz (MA)\n");
+            break;
+        } default:{
+            printf("DDD nao cadastrado\n");
+            break;
+        }
+    }
+    return 0;
+}
