e6ee238f1025362ce0881d7263c811075537f99f
Gildean.Everett <gmonteiro0808@gmail.com>
2025-07-24 12:05:23 -0300

    programando idade em dias

---
+++ Programas simples/Idade_em_dias.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main() {
+    int dias, anos, meses;
+
+    printf("Digite a idade em dias: \n");
+    scanf("%i",&dias);
+
+    anos = dias / 365;
+    dias %= 365;
+    meses = dias / 30;
+    dias %= 30;
+    
+    printf("%i ano(s)\n", anos);
+    printf("%i mes(es)\n", meses);
+    printf("%i dia(s)\n", dias);
+  
+    return 0;
+}
