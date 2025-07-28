0ae1375770bdb2b08dc9b85bc6ecabdf5a79b722
Gildean.Everett <gmonteiro0808@gmail.com>
2025-07-28 13:25:21 -0300

    arruando erros ortograficos

--- Programas simples/Diferenca.c
+++ Programas simples/Diferenca.c
@@ -2,8 +2,8 @@
 
 int main() {
    
-    int valor1, valor2, valor3, valor4, direfenca;
-    //É possivel utilizar tanto %i quanto %d para escanear uma variavel.
+    int valor1, valor2, valor3, valor4, diferenca;
+    //É possivel utilizar tanto %i quanto %d para escanear uma variavel de valor inteiro.
     scanf("%i %i", &valor1, &valor2); 
     scanf("%d %d", &valor3, &valor4);
 
